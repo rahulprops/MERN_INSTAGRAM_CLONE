fce869ef773cb37004ebf66dd0e5cd719d31f1ec
rahulprops <rahulhack912@gmail.com>
2025-01-30 14:50:57 +0530

    likes and disliked complate

--- backend/controller/post.controller.js
+++ backend/controller/post.controller.js
@@ -2,7 +2,8 @@ import sharp from 'sharp';
 import errorHandler from '../middleware/error_logs/errorHandler.js'
 import cloudinary from '../utils/cloudinary.js';
 import postModel from '../models/post.model.js';
- 
+import userModel from '../models/user.model.js';
+ //! create post
 export const addNewPost=async (req,res)=>{
     const {caption}=req.body;
     const image=req.file;
@@ -25,8 +26,18 @@ export const addNewPost=async (req,res)=>{
             image:cloudResponse.secure_url,
             author:autherId
         })
+
+        const user=await userModel.findById(autherId)
+        
         if(post){
+            if(user){
+                user.posts.push(post._id)
+                await user.save()
+            }
+            
             await post.save()
+
+            await post.populate({path:'author', select:"-password"})
             return errorHandler(res,201,"post create sucess",post)
         }else{
             return errorHandler(res,400,"post create failed")
@@ -36,3 +47,78 @@ export const addNewPost=async (req,res)=>{
         return errorHandler(res,500,`server error ${err.message}`)
     }
 }
+//! getall post
+ export const getAllPost=async (req,res)=>{
+    try {
+        const posts=await postModel.find().sort({createAt:-1}).populate({path:'author',select:'username,profilePicture'})
+        .populate({
+            path:"comments",
+            sort:{createdAt:-1},
+            populate:{
+                path:"author",
+                select:'username , profilePicture'
+            }
+        })
+        return errorHandler(res,200,"get post",posts)
+    } catch (err) {
+        return errorHandler(res,500,`server error ${err.message}`)
+    }
+ }
+ //!get userspost
+ export const getUserPost=async (req,res)=>{
+    try {
+        const authorId=req.id;
+        const posts=await postModel.find({author:authorId}).sort({createAt:-1}).populate({
+            path:"author",
+            select:"username,profilePicture"
+        })
+        .populate({
+            path:"comments",
+            sort:{createdAt:-1},
+            populate:{
+                path:"author",
+                select:'username , profilePicture'
+            }
+        })
+        return errorHandler(res,200,"user posts",posts)
+    } catch (err) {
+        return errorHandler(res,500,`server error ${err.message}`)
+    }
+ }
+
+ //! likes post
+ export const likepost=async (req,res)=>{
+    try {
+        const userId=req.id;
+        const postId=req.params.id;
+        const post = await postModel.findById(postId)
+        if(!post){
+            return errorHandler(res,404,"post not found")
+        }
+        await post.updateOne({$addToSet:{likes:userId}})
+        await post.save()
+        // implement socket io for real time notfication
+        return errorHandler(res,200,"post liked")
+    } catch (error) {
+        return errorHandler(res,500,`server error ${err.message}`)
+    }
+ }
+ //!unlike post
+ export const unlikePost=async (req,res)=>{
+    try {
+         const userId=req.id;
+         const postId=req.params.id;
+         const post = await postModel.findById(postId)
+         if(!post){
+            return errorHandler(res,404,"post not found")
+         }
+         await post.updateOne({$pull:{likes:userId}})
+         await post.save()
+             // implement socket io for real time notfication
+             
+             return errorHandler(res,200,"post disliked")
+
+    } catch (err) {
+        return errorHandler(res,500,`server error ${err.message}`)
+    }
+ }
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,8 +1,12 @@
 import express from 'express'
-import { addNewPost } from '../controller/post.controller.js'
+import { addNewPost, getAllPost, likepost, unlikePost } from '../controller/post.controller.js'
 import isAuthenticated from '../middleware/auth/isAuthenticated.js'
 import upload from '../middleware/multer/multer.js'
 const postRouter=express.Router()
 
 postRouter.post("/create",isAuthenticated,upload.single("image"),addNewPost)
+postRouter.get("/all-posts", getAllPost)
+postRouter.get("/user-posts", isAuthenticated,getAllPost)
+postRouter.post("/likes/:id", isAuthenticated,likepost)
+postRouter.post("/dislike/:id", isAuthenticated, unlikePost)
 export default postRouter;
