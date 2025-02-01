c6438b73824565d404c0b817c335606471034c6d
rahulprops <rahulhack912@gmail.com>
2025-02-01 14:29:06 +0530

    redux toolkit persist setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Provider} from 'react-redux'
-import store from './redux/store.js'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import store, { persistor } from './redux/store.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <Provider store={store}>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,13 +1,34 @@
-import { configureStore} from '@reduxjs/toolkit'
-import authSlice from './slice/authSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import authSlice from './slice/authSlice';
 import apiSlice from './apis/apiSlice';
 
-const store=configureStore({
-    reducer:{
-        auth:authSlice,
-        [apiSlice.reducerPath]:apiSlice.reducer
-    },
+// Persist Config
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['auth'], // Persist only the auth slice
+};
+
+// Combine Reducers
+const rootReducer = combineReducers({
+    auth: authSlice,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+});
+
+// Persist Reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure Store
+const store = configureStore({
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(apiSlice.middleware),
-})
+        getDefaultMiddleware({
+            serializableCheck: false, // Required for redux-persist
+        }).concat(apiSlice.middleware),
+});
+
+export const persistor = persistStore(store);
 export default store;
